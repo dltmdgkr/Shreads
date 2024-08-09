30d98e5a792839de2e7c3868f539fbfaca6f8b34
dltmdgkr <lseunghak1301@gmail.com>
2024-08-09 09:34:03 +0900

    유저 프로필 페이지 팔로우 기능 추가

--- src/app/(afterLogin)/(profile)/[userId]/_component/UserInfo.tsx
+++ src/app/(afterLogin)/(profile)/[userId]/_component/UserInfo.tsx
@@ -2,19 +2,92 @@
 
 import Link from "next/link";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getUserById } from "../../../messages/_lib/getUserById";
 import { useFetchUser } from "@/app/(afterLogin)/_hook/useFetchUser";
+import { useEffect, useState } from "react";
+import { isFollowingUser } from "@/app/(afterLogin)/explore/_lib/isFollowingUser";
+import { followUser } from "@/app/(afterLogin)/explore/_lib/followUser";
+import { getFollowerCount } from "@/app/(afterLogin)/explore/_lib/getFollowerCount";
+
+type FollowerData = {
+  follower_count: number;
+};
 
 export default function UserInfo({ userId }: { userId: string }) {
-  const { user } = useFetchUser();
+  const { user: me } = useFetchUser();
+  const queryClient = useQueryClient();
+
+  const [isFollowing, setIsFollowing] = useState<boolean | null>(null);
 
   const { data } = useQuery({
     queryKey: ["users", userId],
-    queryFn: () => getUserById(userId),
+    queryFn: async () => await getUserById(userId),
+  });
+
+  useEffect(() => {
+    const fetchFollowStatus = async () => {
+      if (me?.id && data?.id) {
+        const followingStatus = await isFollowingUser(data.id, me.id);
+        setIsFollowing(followingStatus);
+      }
+    };
+
+    fetchFollowStatus();
+  }, [me?.id, data?.id]);
+
+  const followMutationQuery = useMutation({
+    mutationKey: ["users", "follows"],
+    mutationFn: async () => {
+      if (data && me?.id) {
+        await followUser(data.id, me.id, isFollowing);
+      }
+    },
+    onMutate: async () => {
+      const queryKey = ["users", "follows"];
+
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousData = queryClient.getQueryData<FollowerData>(queryKey);
+
+      queryClient.setQueryData<FollowerData>(queryKey, (oldData) => ({
+        ...oldData,
+        follower_count: (oldData?.follower_count || 0) + (isFollowing ? -1 : 1),
+      }));
+
+      return { previousData };
+    },
+    onError: (err, variables, context) => {
+      const queryKey = ["users", "follows"];
+      queryClient.setQueryData(queryKey, context?.previousData);
+      console.error("Error updating follow:", err);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["users", "follows"],
+      });
+    },
+  });
+
+  const onClickFollow = () => {
+    followMutationQuery.mutate();
+    setIsFollowing((prev) => !prev);
+  };
+
+  const { data: followerData } = useQuery<FollowerData>({
+    queryKey: ["users", "follows"],
+    queryFn: async () => {
+      if (!data) return { follower_count: 0 };
+      const followerCount = await getFollowerCount(data.id);
+      return { follower_count: followerCount };
+    },
   });
 
   if (!data) return null;
+
+  if (isFollowing === null) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -32,19 +105,30 @@ export default function UserInfo({ userId }: { userId: string }) {
       <div className="flex flex-start mb-8">
         <div className="flex flex-1">
           <div className="text-gray-400">팔로워</div>
-          <div className="text-gray-400 ml-1">100명</div>
+          <div className="text-gray-400 ml-1">
+            {followerData?.follower_count || 0}명
+          </div>
         </div>
         <Link href="https://github.com/dltmdgkr" target="_blank">
           <GitHubIcon />
         </Link>
       </div>
-      {user.id === userId ? (
+      {me?.id === userId ? (
         <Link href="/edit-profile">
           <button className="w-full border border-gray-300 px-4 py-1 rounded-xl text-gray-700 hover:bg-gray-100">
             프로필 편집
           </button>
         </Link>
-      ) : null}
+      ) : (
+        <button
+          onClick={onClickFollow}
+          className={`w-full border border-gray-300 px-4 py-1 rounded-xl hover:bg-gray-100 ${
+            isFollowing ? "text-gray-500" : "text-black"
+          }`}
+        >
+          {isFollowing ? "팔로잉" : "팔로우"}
+        </button>
+      )}
     </>
   );
 }
