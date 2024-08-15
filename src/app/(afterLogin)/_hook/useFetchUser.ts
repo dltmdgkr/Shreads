f94bc7f8d2bd07efa69e0e64f07eb3d2241d4088
dltmdgkr <lseunghak1301@gmail.com>
2024-08-15 17:47:02 +0900

    React Query를 이용하여 useFetchLikes 훅을 작성

--- src/app/(afterLogin)/_hook/useFetchUser.ts
+++ src/app/(afterLogin)/_hook/useFetchUser.ts
@@ -1,40 +1,32 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const supabase = createClientComponentClient();
+
+async function fetchUser() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    if (error) throw error;
+    return data;
+  }
+
+  throw new Error("User not found");
+}
 
 export function useFetchUser() {
-  const supabase = createClientComponentClient();
-  const [user, setUser] = useState({
-    avatar_url: "",
-    user_name: "",
-    id: "",
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchUser(),
   });
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchAvatar = async () => {
-      setLoading(true);
-      try {
-        const {
-          data: { user },
-        } = await supabase.auth.getUser();
-        if (user) {
-          const { data, error } = await supabase
-            .from("profiles")
-            .select("*")
-            .eq("id", user.id)
-            .single();
-          if (error) throw error;
-          if (data) setUser(data);
-        }
-      } catch (error) {
-        console.error("Error fetching avatar:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAvatar();
-  }, [supabase]);
-
-  return { user, loading };
+  return { user, loading: isLoading };
 }
