d5e2dd6ce813053bba3a4586d3d17d7b4848768b
dltmdgkr <lseunghak1301@gmail.com>
2024-08-15 17:47:39 +0900

    React Query를 이용하여 useFetchLikes 훅을 작성

---
+++ src/app/(afterLogin)/_hook/useFetchLikes.ts
@@ -0,0 +1,30 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchLikeStatus = async (userId: string, postId: number) => {
+  if (!userId || !postId) {
+    throw new Error("Invalid userId or postId");
+  }
+
+  const supabase = createClientComponentClient();
+  const { data: likeData, error } = await supabase
+    .from("likes")
+    .select("*")
+    .eq("post_id", postId)
+    .eq("user_id", userId)
+    .maybeSingle();
+
+  if (error) {
+    throw new Error("Error fetching like status: " + error.message);
+  }
+
+  return !!likeData;
+};
+
+export function useFetchLikes(userId: string, postId: number) {
+  return useQuery({
+    queryKey: ["likes", userId, postId],
+    queryFn: () => fetchLikeStatus(userId, postId),
+    enabled: !!userId && !!postId,
+  });
+}
